0156d263d8d3e120e93cf3a5911b704c2005effd
Nick Yazdani <nnyazdan@deakin.edu.au>
2020-12-21 19:30:22 +1100

    third commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-offline",
     "gatsby-transformer-sharp",
     `gatsby-plugin-fontawesome-css`,
